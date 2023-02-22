9ebf4c6b90faf142ee45bdb1e41962d102a91708
Joe Paul <joemiddie13@gmail.com>
2023-02-22 13:16:48 -0800

    TEST TEST TEST

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Ford", "Mustang", 2022, "red");
+
+        car1.Drive();
+
+        Console.ReadKey();
+    }
+}
+class Car
+{
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public Car(String make, String model, int year, String color)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+    public void Drive()
+    {
+        Console.WriteLine("You drive the " + make + " " + model);
+    }
+}
