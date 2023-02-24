df0bfd4e12a8c86e188f20889a4b812e5edc4f32
Joe Paul <joemiddie13@gmail.com>
2023-02-23 20:46:54 -0800

    Learning about CONSTRUCTORS

--- app/Program.cs
+++ app/Program.cs
@@ -1,32 +1,16 @@
-﻿class Program
+﻿namespace CSharpIntermediate
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+    }
+
+class Program
 {
     static void Main(string[] args)
     {
-        Car car1 = new Car("Ford", "Mustang", 2022, "red");
-        Car car2 = new Car("Chevy", "Corvette", 2021, "blue");
-
-        car1.Drive();
-        car2.Drive();
-
-        Console.ReadKey();
+        
     }
 }
-class Car
-{
-    String make;
-    String model;
-    int year;
-    String color;
-
-    public Car(String make, String model, int year, String color)
-    {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-    }
-    public void Drive()
-    {
-        Console.WriteLine("You drive the " + make + " " + model);
-    }
 }
