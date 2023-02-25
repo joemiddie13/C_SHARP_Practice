f2821bc7e4c5f6df11c007402ea18e6b460c60dc
Joe Paul <joemiddie13@gmail.com>
2023-02-25 15:19:50 -0800

    Practicing C# Inheritance

--- app/Program.cs
+++ app/Program.cs
@@ -1,16 +1,43 @@
-﻿namespace CSharpIntermediate
-{
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-    }
+﻿using System;
 
-class Program
+namespace MyFirstProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        
+        static void Main(string[] args)
+        {
+            //inheritance = 1 or more child classes recieving fields, methods, etc. from a common parent
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            Console.WriteLine(car.go);
+
+            Console.ReadKey();
+        }
     }
-}
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+    class Car: Vehicle
+    {
+        public int wheels = 4;
+    }
+    class Bicycle: Vehicle
+    {
+        public int wheels = 2;
+    }
+    class Boat: Vehicle
+    {
+        public int wheels = 0;
+    }
 }
